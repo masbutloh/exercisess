d20a1ab223b5b90a28690d9be4a12cc016d72bef
Matthew Johannes <masbutloh@Matthews-Air.localdomain>
2018-07-23 21:45:26 +0700

    finishing touches

---
+++ exercise.js
@@ -0,0 +1,14 @@
+function square (number) {
+    return number*number
+  }
+  
+function pi () {
+    return Math.PI
+  }
+
+
+
+var area = pi () * square(7);
+console.log(pi())
+console.log(square(7))
+console.log(area); // 154
